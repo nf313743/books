3033a6540474c9c41fbdf0531fd5ac1b0f7523eb
nf313743 <1612339+nf313743@users.noreply.github.com>
2017-12-02 12:21:30 +0000

    Ex2

---
+++ functional_programming_in_csharp/chapter_01/Ex2.cs
@@ -0,0 +1,33 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace chapter_01
+{
+    [TestClass]
+    public class Ex2_Test
+    {
+        [TestMethod]
+        public void IsEven()
+        {
+            Assert.IsTrue(Ex2.IsEven(10));
+            Assert.IsFalse(Ex2.IsEven(11));
+        }
+
+        [TestMethod]
+        public void Negate()
+        {
+            var isEvenNeg = Ex2.Negate(Ex2.IsEven);
+            Assert.IsFalse(isEvenNeg(10));
+            Assert.IsTrue(isEvenNeg(11));
+        }
+    }
+
+    public class Ex2
+    {
+        public static Func<int, bool> IsEven 
+            => x => x % 2 == 0;
+
+        public static Func<T, bool> Negate<T>(Func<T, bool> pred) 
+            => x => !pred(x);
+    }
+}
