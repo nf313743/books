6f3225a54b7fad5f65572d52692f1f91ac2ca488
nf313743 <1612339+nf313743@users.noreply.github.com>
2018-05-22 18:38:47 +0100

    temp

--- functional_programming_in_csharp/chapter_06/csharp/Ex01.cs
+++ functional_programming_in_csharp/chapter_06/csharp/Ex01.cs
@@ -1,3 +1,11 @@
+/*
+Write a ToOption extension method to convert an Either into an Option ; the
+left value is thrown away if present. Then write a ToEither method to convert
+an Option into an Either , with a suitable parameter that can be invoked to
+obtain the appropriate Left value if the Option is None . (Tip: start by writing
+the function signatures in arrow notation.)
+ */
+
 using System;
 using NUnit.Framework;
 using LaYumba.Functional;
@@ -36,4 +44,10 @@ public static class EitherExt2
             _ => None,
             x => Some(x)
         );
+
+    public static Either<L, R> ToEither<L,R>(this Option<R> @this)
+        => @this.Match(
+            x => Left(default(L)),
+            y => Right(y)
+        )
 }
