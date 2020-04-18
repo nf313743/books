21e815f3939409e0bd724a252c712093e4ca1acc
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-04-18 09:26:03 +0100

    Lesson 11 completed

--- get_programming_with_fsharp/lesson-11/scratchpad.fsx
+++ get_programming_with_fsharp/lesson-11/scratchpad.fsx
@@ -1,14 +1,13 @@
 open System
 open System.IO
-open System.IO
 
-let writeToFile (date:DateTime) fileName text =
-    let textFileName = sprintf "%s-%s.txt" (date.ToString("yyyyMMdd")) fileName
-    System.IO.File.WriteAllText("./" + textFileName, text)
+let writeToFile (date: DateTime) filename text =
+    let fileName = date.ToString("yyyy-MM-dd") + "--" + filename + ".txt"
+    System.IO.File.WriteAllText("./" + fileName, text)
 
 
 let writeToToday = writeToFile DateTime.UtcNow.Date
-let writeToTomorrow = writeToFile (DateTime.UtcNow.Date.AddDays 1.0)
+let writeToTomorrow = writeToFile (DateTime.UtcNow.Date.AddDays 1.)
 let writeToTodayHelloWorld = writeToToday "hello-world"
 
 writeToToday "first-file" "The quick brown fox jumped over the lazy dog"
@@ -16,7 +15,7 @@ writeToTomorrow "second-file" "The quick brown fox jumped over the lazy dog"
 writeToTodayHelloWorld "The quick brown fox jumped over the lazy dog"
 
 
-// Listing 6.7
+
 let drive distance petrol =
     if distance = "far" then petrol / 2.0
     elif distance = "medium" then petrol - 10.0
@@ -24,15 +23,21 @@ let drive distance petrol =
 
 let startPetrol = 100.0
 
-startPetrol
-|> drive "far"
-|> drive "medium"
-|> drive "short"
-|> printfn "%f"
+let result =
+    startPetrol
+    |> drive "far"
+    |> drive "medium"
+    |> drive "short"
 
-let checkCurrentDictionaryAge =
+let checkCreation (date: DateTime) =
+    match date.Year with
+    | x when x < 2018 -> "old"
+    | _ -> "not old"
+
+// Compose into a single function as the inputs and outputs match
+let checkCurrentDirectoryAge =
     Directory.GetCurrentDirectory
     >> Directory.GetCreationTime
-    
+    >> checkCreation
 
-
+let description = checkCurrentDirectoryAge()
