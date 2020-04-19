878868ee80d16f9c3b7df157f0f1c5bddcd27e26
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-04-19 16:21:16 +0100

    Lesson 15 completed

--- get_programming_with_fsharp/lesson-15/scratchpad.fsx
+++ get_programming_with_fsharp/lesson-15/scratchpad.fsx
@@ -1,45 +1,44 @@
 open System.IO
 open System
+
 let myprint x = printfn "%A" x
 
-type FootballResult = 
-    {
-        HomeTeam: string
-        AwayTeam:string
-        HomeGoals:int
-        AwayGoals:int
-    }
+type FootballResult =
+    { HomeTeam: string
+      AwayTeam: string
+      HomeGoals: int
+      AwayGoals: int }
 
-let create (ht, hg) (at,ag) = 
-    {HomeTeam =ht; AwayTeam = at; HomeGoals = hg; AwayGoals = ag;}
-   
-let results = 
-    [
-        create ("Messiville", 1) ("Ronaldo City", 2)
-        create ("Messiville", 1) ("Bale Town", 3)
-        create ("Bale Town", 3) ("Ronaldo City", 1)
-        create ("Bale Town", 2) ("Messiville", 1)
-        create ("Ronaldo City", 4) ("Messiville", 2)
-        create ("Ronaldo City", 1) ("Bale Town", 2)
-    ]
+let create (ht, hg) (at, ag) =
+    { HomeTeam = ht
+      AwayTeam = at
+      HomeGoals = hg
+      AwayGoals = ag }
 
-let myResult1 = 
-    results |>
-    List.filter (fun x -> x.AwayGoals > x.HomeGoals)
-    |> List.groupBy(fun x-> x.AwayTeam)
-    |> List.map(fun (x, y)-> (x, List.length y ))
-    |> List.sortByDescending(fun (_, y) -> y)
-    |> myprint
+let results =
+    [ create ("Messiville", 1) ("Ronaldo City", 2)
+      create ("Messiville", 1) ("Bale Town", 3)
+      create ("Bale Town", 3) ("Ronaldo City", 1)
+      create ("Bale Town", 2) ("Messiville", 1)
+      create ("Ronaldo City", 4) ("Messiville", 2)
+      create ("Ronaldo City", 1) ("Bale Town", 2) ]
 
-
-results |>
-List.filter (fun x -> x.AwayGoals > x.HomeGoals)
-|> List.countBy (fun x-> x.AwayTeam)
-|> List.sortByDescending(fun (_, y) -> y)
+// First attempt
+results
+|> List.filter (fun x -> x.AwayGoals > x.HomeGoals)
+|> List.groupBy (fun x -> x.AwayTeam)
+|> List.map (fun (x, y) -> (x, (y.Length)))
 |> myprint
 
-let list1= [ 1; 2; 3; 4; 6 ]
-let list2=[ 1; 2; 3; 4; 6 ]
+// Book's answer
+let isAwayWin result = result.AwayGoals > result.HomeGoals
+results
+|> List.filter isAwayWin
+|> List.countBy(fun x -> x.AwayTeam)
+|> List.sortByDescending(fun (_, x) -> x)
+|> myprint
 
-let t = List.map2 (fun x y -> x * y) list1 list2
-
+// Arrays are mutable
+let numbersArray = [| 1; 2; 3; 4; 6 |]
+numbersArray.[0] <- 99
+numbersArray |> myprint
