1b200a34ab66eab0481d58e263c7cafe265be5a4
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-06-14 15:58:55 +0100

    Lesson 27 completed

---
+++ get_programming_with_fsharp/lesson-27/CSharpApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FSharpCode;
+
+namespace CSharpApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car(4, "Supacars", Tuple.Create(3.5, 3.5));
+            System.Console.WriteLine(car.Brand);
+
+            var motorBike = FSharpCode.Vehicle.NewMotorbike("Yamaha", 4.5) as  FSharpCode.Vehicle.Motorbike;
+            System.Console.WriteLine(motorBike.Name);
+
+            var somewheeledCar =  FSharpCode.Functions.CreateCar(4, "Supacars", 1.5, 3.5);  
+            var fourWheeledCar =  FSharpCode.Functions.CreateFourWheeledCar                              
+                       .Invoke("Supacars")
+                       .Invoke(1.5)
+                       .Invoke(3.5);
+        }
+    }
+}
---
+++ get_programming_with_fsharp/lesson-27/FSharpCode/Library.fs
@@ -0,0 +1,21 @@
+﻿namespace FSharpCode
+
+type Car =
+    { Wheels: int
+      Brand: string
+      Dimensions: float * float }
+
+type Vehicle =
+    | Motorcar of Car
+    | Motorbike of Name: string * EngineSize: float
+
+
+
+module Functions =
+
+    let CreateCar wheels brand x y =
+        { Wheels = wheels
+          Brand = brand
+          Dimensions = x, y }
+
+    let CreateFourWheeledCar = CreateCar 4
