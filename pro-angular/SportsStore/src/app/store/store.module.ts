3b36489bef64af5fd2221b848e5d93c2eca277f5
nf313743 <1612339+nf313743@users.noreply.github.com>
2020-12-07 15:57:46 +0000

    Angular Pro - Chapter 07 completed

---
+++ pro-angular/SportsStore/src/app/store/store.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { ModelModule } from '../model/model.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { StoreComponent } from './store.component';
+import { FormsModule } from '@angular/forms';
+import { CounterDirective } from './counter.directive';
+
+@NgModule({
+  imports: [ModelModule, BrowserModule, FormsModule],
+  declarations: [StoreComponent, CounterDirective],
+  exports: [StoreComponent],
+})
+export class StoreModule {}
